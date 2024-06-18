ab88172b3904b0e5ba63cb67bbc3f84d50df0a87
ovk-lviv <ovk.16.07.2019@gmail.com>
2024-06-18 22:09:05 +0300

    hometask9

--- src/main/java/Man.java
+++ src/main/java/Man.java
@@ -1,2 +1,37 @@
-public class Man {
+public class Man extends Person {
+
+    public Man(String firstName, String lastName, int age, boolean partner) {
+        super(firstName, lastName, age, partner);
+
+    }
+
+    public String getLastName() {
+        return super.getLastName();
+
+    }
+
+    public int getAge() {
+        return super.getAge();
+
+    }
+
+    public boolean isPartner() {
+        return super.isPartner();
+    }
+
+    public boolean isRetired() {
+        if (this.getAge() > 65) {
+            return true;
+        }
+        return false;
+    }
+
+    public void registerPartnership() {
+        setPartner(true);
+    }
+
+    public void deregisterPartnership() {
+        setPartner(false);
+    }
+
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,33 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private boolean partner;
+
+    public Person(String firstName, String lastName, int age, boolean partner) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.partner = partner;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isPartner() {
+        return partner;
+    }
+
+    public void setPartner(boolean partner) {
+        this.partner = partner;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
